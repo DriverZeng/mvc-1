8e60adc7a1ff9f3f439f079bce286bb3f6d59a7e
zls <123@qq.com>
2020-05-13 21:38:28 +0800

    lalala

---
+++ src/app3.js
@@ -0,0 +1,6 @@
+import $ from 'jquery'
+const $square = $('#app3 .square')
+
+$square.on('click',()=>{
+    $square.toggleClass('active')
+})
---
+++ src/app4.js
@@ -0,0 +1,7 @@
+import $ from 'jquery'
+const $circle = $('#app4 .circle')
+$circle.on('mouseenter',()=>{
+    $circle.addClass('active')
+}).on('mouseleave',()=>{
+    $circle.removeClass('active')
+})
